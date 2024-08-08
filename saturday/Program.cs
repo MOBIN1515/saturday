ad5a8066ba466d980edb025ce950b61151ec08f0
Mobin <Mobin@DESKTOP-NIS2M55>
2024-08-08 22:54:24 +0330

    Add project files.

---
+++ saturday/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace saturday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname, lastname, age;
+
+
+            Console.Write("please enter your first name :");
+            firstname = Console.ReadLine();
+
+            Console.Write("please enter your last name : ");
+            lastname = Console.ReadLine();
+
+            Console.Write("please enter your age : ");
+            age = Console.ReadLine();
+
+
+
+
+
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("your first name is : " + firstname);
+            Console.WriteLine("your last name is :" + lastname);
+            Console.WriteLine("your age is :" + age);
+            Console.WriteLine("welcome to the visual stadio");
+
+        }
+    }
+}
